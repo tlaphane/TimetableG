e085b4b792f81af97f3367664edeaa683dad1a6c
tshepang motaung <1431795@host006-08-10.ms.wits.ac.za>
2019-05-10 00:03:26 +0200

    Running tests

--- Timetable/tests/tests.py
+++ Timetable/tests/tests.py
@@ -9,3 +9,20 @@ class TestUrls(TestCase):
         url = reverse('login')
 
         self.assertEquals(resolve(url).func, 'login')
+
+    def test_register_url_resolved(self):
+        url = reverse('register')
+
+        self.assertEquals(resolve(url).func, 'register')
+
+    def test_Confirm_log(self):
+        url = reverse('courses')
+        self.assertEquals(resolve(url).func, 'courses')
+
+    def test_forgot(self):
+        url = reverse('forgot')
+        self.assertEquals(resolve(url).func, 'forgot')
+
+    def test_reset(self):
+        url = reverse('reset')
+        self.assertEquals(resolve(url).func, 'resetp')
