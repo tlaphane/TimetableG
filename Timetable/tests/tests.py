1fee3c8ee8fd36b45be22506dd48efd5756fabf6
tshepang motaung <1431795@host006-08-10.ms.wits.ac.za>
2019-05-09 20:36:37 +0200

    Running tests

--- Timetable/tests/tests.py
+++ Timetable/tests/tests.py
@@ -3,11 +3,16 @@ from django.test import SimpleTestCase
 
 # Create your tests here.
 
-class TestUrls(TestCase):
+'''class TestUrls(TestCase):
     def test_login_url_resolved(self):
         from django.urls import reverse
         url = reverse('login')
 
         from django.urls import resolve
         from Timetable.Register.views import login
-        self.assertEquals(resolve(url).func, login)
+        self.assertEquals(resolve(url).func, login)'''
+
+class TestUrls(SimpleTestCase):
+
+    def test_list_url_is_resolved(self):
+        assert 1 == 2
