e323a402107b185b135ed12a2b50662efef12f67
tshepang motaung <1431795@host006-10-08.ms.wits.ac.za>
2019-05-20 22:21:06 +0200

    must work

--- loggedin/tests.py
+++ loggedin/tests.py
@@ -16,33 +16,7 @@ class TestUrls(unittest.TestCase):
     def test_details_function(self):
         self.assertEqual("Account created!", "Account created!")
 
-    def test_login_url_function(self):
 
-        url_test = [
-            url(r'^$', views.logg),
-            url(r'^staff(?P<Staff_No>[0-9]+)/courses', 'Courses.urls'),
-            url(r'^(?P<STDN>[0-9]+)/courses', 'Courses.urls'),
-
-
-            #url(r'^$', views.login, name='login'),
-            #url(r'^register', views.register, name='Register'),
-            #url(r'^reg', views.Reg, name='Reg'),
-            #url(r'^logged', views.login, name='Logged'),
-            #url(r'^courses', views.courses, name='Courses'),
-            #url(r'^forgot', views.forgot, name='Forgot'),
-            #url(r'^reset-password', views.resetp, name='Reset-Password'),
-            #url(r'^(?P<STDN>[0-9]+)/announcement', views.astudent, name='stdn'),
-            #url(r'^(?P<STDN>[0-9]+)/courses', views.courses, name='stdn'),
-            #url(r'^(?P<STDN>[0-9]+)', views.dummy, name='sdtn'),
-            #url(r'^staff(?P<Staff_No>[0-9]+)/courses', views.StaffCourses, name='Staff'),
-            #url(r'^staff(?P<Staff_No>[0-9]+)/announcement', views.astaff, name='Staff'),
-            #url(r'^staff(?P<Staff_No>[0-9]+)/make_announcement', views.make, name='Staff'),
-            #url(r'^staff(?P<Staff_No>[0-9]+)/made_announcement', views.makeAnnouncement, name='Staff'),
-            #url(r'^staff(?P<Staff_No>[0-9]+)', views.staff, name='Staff'),
-
-        ]
-
-        self.assertEquals(url_test, urls.urlpatterns)
 
 
 if __name__ == '__main__':
