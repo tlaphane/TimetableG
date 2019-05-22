96158b88101db906dc564a2d62cc404eaad5c263
Motaung08 <50378344+Motaung08@users.noreply.github.com>
2019-05-22 06:01:12 +0200

    Update test_views.py

--- loggedin/test_views.py
+++ loggedin/test_views.py
@@ -12,7 +12,11 @@ from django.urls import reverse, resolve
     #def test_confirm_islogin(self):
         #self.assertTrue('Register/Log_in.html'.islogin())
 class TestUrls(SimpleTestCase):
-
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login')
+        self.confirm_log = reverse('confirm_log')
+        
     def test_views_logg_is_resolved(self):
         url = reverse('login')
         self.assertEquals(resolve(url).func, login)
