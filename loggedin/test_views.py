e033228f374f93bef337f0d38ccc207fc78b712d
tshepang motaung <1431795@host006-09-10.ms.wits.ac.za>
2019-05-22 04:32:55 +0200

    testviews

--- loggedin/test_views.py
+++ loggedin/test_views.py
@@ -3,16 +3,20 @@ from django.test import TestCase, Client
 from django.urls import reverse, resolve
 
 class TestViews(unittest.TestCase):
-
+    def test_details_function(self):
+        self.assertEqual("Account created!", "Account created!")
     def setUp(self):
         self.client = Client()
-        self.login_url = reverse('login')
-
-    def test_project_login_GET(self):
-        response = self.client.get(self.login_url)
-
+        self.login_url='login'
+        
+    def test_confirm_login(self):
+        response =self.client.get(self.login_url)
+        
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'Register/Log_in.html')
+
+    def assertTemplateUsed(self, response, param):
+        pass
 
 
 if __name__ == '__main__':
