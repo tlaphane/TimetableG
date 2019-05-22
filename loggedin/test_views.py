8c3a66ce264f6bb51b2e9d2bc82d77a22855bf6c
Motaung08 <50378344+Motaung08@users.noreply.github.com>
2019-05-22 05:52:42 +0200

    Update test_views.py

--- loggedin/test_views.py
+++ loggedin/test_views.py
@@ -1,7 +1,7 @@
 import unittest
 from django.test import SimpleTestCase, Client
 from django.urls import reverse, resolve
-from django.views import *
+from loggedin import views
 
 #class TestViews(unittest.TestCase):
     #def test_details_function(self):
@@ -15,8 +15,8 @@ from django.views import *
 class TestUrls(SimpleTestCase):
 
     def test_views_logg_is_resolved(self):
-        url = reverse("r'^$'")
-        print(resolve(url))
+        url = reverse('login')
+        self.assertEquals(resolve(url).func, login)
 
 if __name__ == '__main__':
     unittest.main()
