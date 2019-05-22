4a1ba7a662d02e91fdfbfee8126033b9eaa2644e
tshepang motaung <1431795@host006-09-10.ms.wits.ac.za>
2019-05-22 06:19:37 +0200

    test_views

--- Register/tests.py
+++ Register/tests.py
@@ -7,25 +7,61 @@ from django.test import  TestCase
 from loggedin import urls
 
 
-# Create your tests here.
-class UrlTestCase(TestCase):
-    def setUp(self):
-        url.objects.create(name="login", page="loggedin")
-        url.objects.create(name="Notlogged", page="loggedoff")
 
-    def test_url_loggedin(self):
-        login = url.objects.create(name="login")
-        Notlogged = url.objects.create(name="Notlogged")
 
-if __name__ == '__init__':
-    unittest.init()
 
-    def test_login_url_resolve(self):
-        url_test = reverse('loggedin'),
-        self.assertEquals(url_test, url.urlpatterns)
+class TestUrls(unittest.TestCase):
+
+    def test_details_function(self):
+        self.assertEqual("Account created!", "Account created!")
+
+    def view_login(self):
+        self.assertEquals('loggedin', 'Register/Loggedin.html')
+
+    def test_isview_login(self):
+        self.assertTrue('login', 'logged')
+        self.assertTrue('login', './')
         
+    def test_isview_notlogin(self):
+        self.assertNotIn('logged', './')
+
+    def test_Confirm_log(self):
+        self.assertTrue('Confirm', 'Confirm')
+        self.assertNotIn('Confirm', './')
+
+
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Create your tests here.
+#class UrlTestCase(TestCase):
+    #def setUp(self):
+        #url.objects.create(name="login", page="loggedin")
+        #url.objects.create(name="Notlogged", page="loggedoff")
+
+    #def test_url_loggedin(self):
+        #login = url.objects.create(name="login")
+        #Notlogged = url.objects.create(name="Notlogged")
+
+
+    #def test_login_url_resolve(self):
+        #url_test = reverse('loggedin'),
+        #self.assertEquals(url_test, url.urlpatterns)
+        
+#if __name__ == '__main__':
+    #unittest.main()
     
     
     
--- loggedin/test_views.py
+++ loggedin/test_views.py
@@ -1,14 +1,26 @@
 import unittest
-from django.test import TestCase, Client
+from django.test import SimpleTestCase, Client
 from django.urls import reverse, resolve
 
-class TestViews(unittest.TestCase):
-    def test_details_function(self):
-        self.assertEqual("Account created!", "Account created!")
-         
-    def test_confirm_login(self):
-        self.assertEqual('login','Register/Log_in.html')
+#class TestViews(unittest.TestCase):
+    #def test_details_function(self):
+        #self.assertEqual("Account created!", "Account created!")
 
+    #def test_confirm_login(self):
+        #self.assertEqual('loggedin'.logg(), 'Register/Log_in.html')
+
+    #def test_confirm_islogin(self):
+        #self.assertTrue('Register/Log_in.html'.islogin())
+class TestUrls(SimpleTestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login')
+        self.confirm_log = reverse('confirm_log')
+
+    def test_views_logg_is_resolved(self):
+        url = reverse('login')
+        self.assertEquals(resolve(url).func, login)
 
 if __name__ == '__main__':
     unittest.main()
