61dcc524da2632b179baefe5c3a98e79b2c6772c
Motaung08 <50378344+Motaung08@users.noreply.github.com>
2019-05-24 20:42:26 +0200

    Update views.py

--- Log_In/views.py
+++ Log_In/views.py
@@ -1,57 +1,6 @@
-from django.contrib import messages
-from django.core.mail import send_mail
-from django.shortcuts import HttpResponse,render, redirect
-from .models import StudentsRegister, Lecturer, RegisteredStaffs
-from django.conf import settings
-from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render
 
 
-def login(request):
-    context = {
-        'staff': Lecturer.objects.all(),
-        'students': StudentsRegister.objects.all(),
+def logg(request):
 
-    }
-
-    return render(request, 'Register/Log_in.html', context)
-
-
-def dummy(request, STDN):
-
-    try:
-        kep = request.POST.get('uname', False)
-        stdin = int(request.POST.get('uname', False))
-        stdnum = stdin
-        pswin = request.POST.get('psw', False)
-        user = StudentsRegister.objects.get(Student_No=stdin, Password=pswin)
-    except StudentsRegister.DoesNotExist:
-        if(kep):
-            return render(request, 'Register/Log_in.html', {'error_message': "Wrong password or Student number", })
-        else:
-            return render(request, 'Register/Log_in.html')
-    else:
-
-        return render(request, 'Register/Loggedin.html', {'STDN': STDN})
-
-
-def staff(request,Staff_No):
-
-    try:
-        kep = request.POST.get('uname', False)
-        stdin = int(request.POST.get('uname', False))
-        stdnum = stdin
-        pswin = request.POST.get('psw', False)
-        user = Lecturer.objects.get(Lect_No=stdin, Password=pswin)
-        user1 = RegisteredStaffs.objects.filter(Staff_no=Staff_No)
-    except StudentsRegister.DoesNotExist:
-        if(kep):
-            return render(request, 'Register/Log_in.html', {'error_message': "Wrong password or Student number", })
-        else:
-            return render(request, 'Register/Log_in.html')
-
-    else:
-
-        return render(request, 'Register/lecturer_page.html', {'STDN': Staff_No,'staff': user1})
-
-
-   # return render(request, 'Register/lecturer_page.html')
+    return render(request, 'loggedin/Loggedin.html')
