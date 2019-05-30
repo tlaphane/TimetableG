58ad8b0c89e9c47c4009cddbef4cadc3ead0630c
Motaung08 <1431795@students.wits.ac.za>
2019-05-30 04:01:17 +0200

    FIX BUILD

--- manage.py
+++ manage.py
@@ -4,15 +4,15 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Timetable.settings')
-    #try:
-        #from django.core.management import execute_from_command_line
-    #except ImportError as exc:
-        #raise ImportError(
-            #"Couldn't import Django. Are you sure it's installed and "
-            #"available on your PYTHONPATH environment variable? Did you "
-            #"forget to activate a virtual environment?"
-        #) from exc
-    #execute_from_command_line(sys.argv)
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        )from exc
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
